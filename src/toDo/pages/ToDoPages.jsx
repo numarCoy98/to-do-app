c794d71ed7d5745bb589541c8524528fe1c5ba45
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-14 12:16:42 -0500

    feat: Boton flotante para añadir tareas

--- src/toDo/pages/ToDoPages.jsx
+++ src/toDo/pages/ToDoPages.jsx
@@ -1,11 +1,26 @@
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { TodoLayout } from "../layout"
-import { CompleteTasksView, ToDoTasksView } from '../views'
+import { ToDoTasksView } from '../views'
 
 export const ToDoPages = () => {
     console.log('Hola mundo')
     return (
         <TodoLayout>
             <ToDoTasksView />
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'blue',
+                    ':hover': { opacity: 0.9, backgroundColor: 'secondary.main' },
+                    backgroundColor: 'primary.main',
+                    position: 'fixed',
+                    right: 30,
+                    bottom: 30
+                }}
+            >
+                <AddOutlined sx={{ fontSize: '30', color: 'white' }} />
+            </IconButton>
         </TodoLayout>
     )
 }
