3bd9b0fcd0d2c0a4b954f588afecd16c5c0ec2af
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-15 11:50:31 -0500

    feat: Filtro de estado y categoria en el componente "SideBar"

--- src/toDo/components/SideBar.jsx
+++ src/toDo/components/SideBar.jsx
@@ -1,7 +1,21 @@
-import { TurnedInNot } from "@mui/icons-material"
+import { TurnedInNot, TurnedIn } from "@mui/icons-material"
 import { Drawer, Box, Toolbar, Typography, Divider, ListItem, ListItemButton, ListItemIcon, List, ListItemText } from "@mui/material"
 
+import { useDispatch, useSelector } from "react-redux"
+import { filterData } from "../../store/slices/todos"
+
+
+const statusString = { 'Finalizadas': 'done', 'Todas': 'all', 'Pendientes': 'pending' }
+
 export const SideBar = ({ drawerWidth = 240 }) => {
+
+    const { categories, filter } = useSelector((state) => state.todo)
+    const dispatch = useDispatch();
+
+    const handleSelectStatus = (key, value) => {
+        dispatch(filterData({ [key]: filter[key] === value ? undefined : value }))
+    }
+
     return (
         <Box
             component='nav'
@@ -21,13 +35,28 @@ export const SideBar = ({ drawerWidth = 240 }) => {
                 <Divider />
                 <List>
                     {
-                        ['Pendientes', 'Finalizadas', 'Todas'].map(text => (
-                            <ListItem key={text} disablePadding>
+                        ['Todas', 'Pendientes', 'Finalizadas'].map(text => (
+                            <ListItem onClick={() => handleSelectStatus('status', statusString[text])} key={text} disablePadding>
                                 <ListItemButton>
                                     <ListItemIcon>
-                                        <TurnedInNot />
+                                        {statusString[text] === filter?.status ? <TurnedIn /> : <TurnedInNot />}
                                     </ListItemIcon>
                                     <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))
+                    }
+                </List>
+                <Divider />
+                <List>
+                    {
+                        categories.map(text => (
+                            <ListItem onClick={() => handleSelectStatus('category', text)} key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {filter.category === text ? <TurnedIn /> : <TurnedInNot />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text.toUpperCase()} />
                                 </ListItemButton>
                             </ListItem>
                         ))
