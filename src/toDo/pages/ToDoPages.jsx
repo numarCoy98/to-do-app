d302fee8f7a80087b8bc33f48aa4da774174ae67
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-15 00:54:09 -0500

    feat: Modal para crear o editar tareas

--- src/toDo/pages/ToDoPages.jsx
+++ src/toDo/pages/ToDoPages.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
 import { AddOutlined } from "@mui/icons-material"
 import { IconButton } from "@mui/material"
+
 import { TodoLayout } from "../layout"
 import { ToDoTasksView } from '../views'
+import { ModalTask } from "../components/ModalTask"
+
 
 export const ToDoPages = () => {
-    console.log('Hola mundo')
+    const [shomModal, setShomModal] = useState(false)
+
+    const handleOpen = () => {
+        setShomModal(true);
+    };
+    const handleClose = () => {
+        setShomModal(false);
+    };
+
     return (
         <TodoLayout>
             <ToDoTasksView />
             <IconButton
+                onClick={handleOpen}
                 size='large'
                 sx={{
                     color: 'blue',
@@ -21,6 +34,11 @@ export const ToDoPages = () => {
             >
                 <AddOutlined sx={{ fontSize: '30', color: 'white' }} />
             </IconButton>
+            {shomModal && <ModalTask
+                handleClose={handleClose}
+                open={shomModal}
+                handleSave={() => console.log('guardar tarea')}
+            />}
         </TodoLayout>
     )
 }
