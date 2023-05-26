a510feb8ee6eeffd665a604e307cbe9402bf5fad
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-26 07:38:03 -0500

    Formulario de la RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,25 @@
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from "../layout"
 import { Link as RouterLink } from 'react-router-dom'
+import { useForm } from "../../hooks/useForm"
+
+const formData = {
+    displayName: '',
+    password: '',
+    email: ''
+}
 
 export const RegisterPage = () => {
+
+    const { email, password, displayName, formState, onInputChange } = useForm(formData)
+
+    const onSubmit = event => {
+        event.preventDefault();
+        console.log({ formState })
+    }
     return (
         <AuthLayout title="Crear cuenta">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField
@@ -13,6 +27,9 @@ export const RegisterPage = () => {
                             type="text"
                             placeholder='Nombre completo'
                             fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -21,6 +38,9 @@ export const RegisterPage = () => {
                             type="email"
                             placeholder='correo@google.com'
                             fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -29,11 +49,14 @@ export const RegisterPage = () => {
                             type="password"
                             placeholder='Contraseña'
                             fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12}>
-                            <Button variant='contained' fullWidth>
+                            <Button type="submit" variant='contained' fullWidth>
                                 Crear cuenta
                             </Button>
                         </Grid>
