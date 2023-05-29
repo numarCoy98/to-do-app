1338cd369aeb06b0c05ffab925c30327cace0acd
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-29 13:17:48 -0500

    feat: vista loading e implementación cuando status este en 'checking'

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,13 @@
 import { Route, Routes } from "react-router-dom"
+import { useSelector } from "react-redux"
 import { AuthRoutes } from "../auth/routes"
 import { TodoRoutes } from "../toDo/routes/TodoRoutes"
+import { CheckingAuth } from "../ui"
 
 export const AppRouter = () => {
+
+    const { status } = useSelector(state => state.auth)
+    if (status === 'checking') return <CheckingAuth />
     return (
         <Routes>
             {/* login y registro */}
