3734948892023da190edf1cb0409e24956a8f674
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-29 14:17:49 -0500

    onAuthStateChanged para revisar que esta conectado

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,39 @@
-import { Route, Routes } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { Route, Routes, Navigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 import { AuthRoutes } from "../auth/routes"
 import { TodoRoutes } from "../toDo/routes/TodoRoutes"
 import { CheckingAuth } from "../ui"
+import { useEffect } from "react"
+import { FirebaseAuth } from "../firebase/config"
+import { onAuthStateChanged } from "firebase/auth";
+import { login, logout } from "../store/slices/auth/authSlice"
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(FirebaseAuth, async (user) => {
+            if (!user) return dispatch(logout());
+            const { uid, displayName, photoURL } = user
+            dispatch(login({ uid, displayName, photoURL }))
+        })
+    }, [])
+
 
     const { status } = useSelector(state => state.auth)
     if (status === 'checking') return <CheckingAuth />
     return (
         <Routes>
-            {/* login y registro */}
-            <Route path="/auth/*" element={<AuthRoutes />} />
-            {/* Todo App */}
-            <Route path="/*" element={<TodoRoutes />} />
+            {status === 'authenticated'
+                // To-do App
+                ? <Route path="/*" element={<TodoRoutes />} />
+                // login y registro
+                : <Route path="/auth/*" element={<AuthRoutes />} />
+            }
+
+            <Route path="/*" element={<Navigate to='/auth/login' />} />
+
         </Routes>
 
     )
